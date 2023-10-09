e78a2c1afbadead764693a42773abf595b133bd3
DESKTOP-3OD72II\yuuta <e1q20070@oit.ac.jp>
2023-10-09 17:47:07 +0900

    game2

--- tempgun/Assets/TempRunScript/PlayerController2.cs
+++ tempgun/Assets/TempRunScript/PlayerController2.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class PlayerController2 : MonoBehaviour {
 
@@ -12,12 +13,34 @@ public class PlayerController2 : MonoBehaviour {
 
 	AudioSource audioSource;
 	public Slider slider;
+	[SerializeField] private Transform _grandChild;
+
+	private float nextTime;
+	public float interval = 0.1f;   // 点滅周期
+
+	Renderer renderComponent;
+
+	private bool flag = true;
+
+
+
+
+
+
+
+
+
+
 
 	void Start()
 	{
 		//Componentを取得
 		audioSource = GetComponent<AudioSource>();
 		slider.onValueChanged.AddListener(value => this.audioSource.volume = value);
+		nextTime = Time.time;
+		renderComponent = transform.Find("Teddy_Bear").gameObject.GetComponent<Renderer>();
+		
+		
 	}
 	void Update () {
         anim.SetBool("jump", Input.GetKey(KeyCode.UpArrow)|| Input.GetKey(KeyCode.JoystickButton0));
@@ -37,16 +60,39 @@ public class PlayerController2 : MonoBehaviour {
 				transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
 			}
 		}
+		
 
 
 	}
 	void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject.tag == "wall")
+		if (flag)
 		{
-			audioSource.PlayOneShot(sound1);
-			Destroy(other.gameObject);
+			if (other.gameObject.tag == "wall")
+			{
+				flag = false;
+				audioSource.PlayOneShot(sound1);
+				Destroy(other.gameObject);
+				StartCoroutine(Blink());
+				
+
+			}
 		}
+	}
+	IEnumerator Blink()
+	{
+
+
+		for (int i = 0; i < 5; i++)
+		{
+			renderComponent.enabled = !renderComponent.enabled;
+			yield return new WaitForSeconds(0.1f);
+			renderComponent.enabled = !renderComponent.enabled;
+			yield return new WaitForSeconds(0.1f);
+
+
+		}
+		flag = true;
 	}
 
 }
